120aebbe25707282e1eb4da771de05290afc4a20
goodshark <gsbeegnnord@icloud.com>
2015-07-07 20:52:05 +0800

    modify pipe.c

--- c/ipc/pipe.c
+++ c/ipc/pipe.c
@@ -1,7 +1,30 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
+    int n, fd[2];
+    pid_t pid;
+    char line[1024];
+
+    if (pipe(fd) < 0) {
+        printf("pipe error\n");
+        exit(-1);
+    }
+    if ((pid = fork()) < 0) {
+        printf("fork error\n");
+        exit(-1);
+    } else if (pid > 0) {
+        // parent
+        close(fd[0]);
+        write(fd[1], "hi, child process\n", 18);
+    } else {
+        // child
+        close(fd[1]);
+        n = read(fd[0], line, 1024);
+        write(STDOUT_FILENO, line, n);
+        //printf("msg: %s", line);
+    }
     return 0;
 }
